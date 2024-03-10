6e7a012ca245a024f4ef88fa59bfe5dcdf375562
brmxy <yarabram111@gmail.com>
2024-03-10 22:30:06 +0700

    chore: setup turborepo, docker compose, web & api project

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	arrowParens: 'avoid',
+	tabWidth: 2,
+	useTabs: true,
+	printWidth: 80
+}
